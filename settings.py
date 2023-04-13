942931c52c404f169969c9e30eda446e2cf1d178
Andrei Ovchinnikov <andrei.s.ovchinnikov@gmail.com>
2023-04-13 14:44:48 +1000

    fullscreen

--- settings.py
+++ settings.py
@@ -7,6 +7,7 @@ class Settings():
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+        self.full_screen = False
         
         # Настройки корабля
         self.ship_speed = 2
