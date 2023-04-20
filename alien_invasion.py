804557738535395436f56fbb3b1bf4624664de73
Andrei Ovchinnikov <andrei.s.ovchinnikov@gmail.com>
2023-04-20 17:11:17 +1000

    ready game

--- alien_invasion.py
+++ alien_invasion.py
@@ -79,6 +79,7 @@ class AlienInvasion:
             self.stats.game_active = True
             self.sb.prep_score()
             self.sb.prep_level()
+            self.sb.prep_ships()
             
             # Очистка списков пришельцев и снарядов
             self.aliens.empty()
@@ -178,6 +179,7 @@ class AlienInvasion:
         if self.stats.ships_left > 1:
             # Уменьшение ships_left.
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             # Очистка списка пришельцев и снарядов.
             self.bullets.empty()
