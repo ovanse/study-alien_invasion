decda450299d7417ae0385216ad078f831ac1f52
Andrei Ovchinnikov <andrei.s.ovchinnikov@gmail.com>
2023-04-20 17:14:11 +1000

    fix bullet width

--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ class Settings():
         self.ship_limit = 3
         
         # Параметры снаряда
-        self.bullet_width = 500
+        self.bullet_width = 5
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 30
